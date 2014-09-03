91917291e753a09d03b6d21a99949f67bc68eb25
judasnow <judasnow@gmail.com>
2014-09-03 13:51:43 +0800

    rethink about yield

--- pylang/async_test.py
+++ pylang/async_test.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from time import sleep
+
 class Pc(object):
     """
      loop
@@ -15,33 +17,44 @@ class Pc(object):
            yield to produce
     """
 
-    _BUFFER_SIZE = 1024
+    _BUFFER_SIZE = 10
     _item_count = 0
 
     _producer_gen = None
     _consumer_gen = None
 
     def _producer(self):
+        # 为了不让函数退出 
+        # 因为函数退出 就没有意义了
         while True:
             if self._item_count < self._BUFFER_SIZE:
                 self._item_count = self._item_count + 1
 
-            # 在整个过程中 
-            yield self._consumer_gen.next()
+            # 在整个过程中 关键的一点就是 yield 之后的表达式 是可以返回
+            # 的 因此就有了 callee 和 协程之间的 通信渠道 
+            #（另一条是 使用全局变量）
+            sleep(2)
+            print "will yield to consumer, and _item_count={0}".format(self._item_count)
+            yield self._consumer_gen
 
     def _consumer(self):
         while True:
             if self._item_count > 0:
                 self._item_count = self._item_count - 1
 
-            yield self._producer_gen.next()
+            sleep(2)
+            print "will yield to producer, and _item_count={0}".format(self._item_count)
+            yield self._producer_gen
 
     def __init__(self):
+        # 生成相应的生成器
         self._producer_gen = self._producer()
         self._consumer_gen = self._consumer()
 
     def run(self):
-        self._producer_gen.next()
+        gen = self._producer_gen
+        while True:
+            gen = gen.next()
 
 if __name__ == "__main__":
     pc = Pc()
