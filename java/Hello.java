0f89da20ed7c0a0d9e5f6c86a2588421a65fee88
judasnow <judasnow@gmail.com>
2014-10-08 20:22:51 +0800

    java awt

---
+++ java/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+// Save as "Hello.java"
+   public static void main(String[] args) {
+      System.out.println("Hello, world!");   // print message
+   }
+}
---
+++ java/maytweepee/src/main.java
@@ -0,0 +1,5 @@
+public class main {
+    static void Main(){
+
+    }
+}
