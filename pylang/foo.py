f7e154b786b5862441ddb11d4adee620d967e0e2
judasnow@gmail.com <judasnow>
2014-12-22 13:27:46 +0800

    py metaclass

--- pylang/foo.py
+++ pylang/foo.py
@@ -9,15 +9,31 @@ def test_metaclass(name, bases, dict):
     # str is not callable
     return "yellow"
 
+    
+def init_attributes(name, bases, dict):
+    if 'attributes' in dict:
+        for attr in dict['attributes']:
+            dict[attr] = None
+
+    return type(name, bases, dict)
+    
+class MetaSingleton(type):
+    instance = None
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(MetaSingleton, cls).__call__(*args, **kw)
+        return cls.instance
+
 class M(type):
 
     def __new__(cls, name, bases, attrs):
         newattrs = dict(attrs, action_list=attrs["action_cooldown"].keys())
         return super(M, cls).__new__(cls, name, bases, newattrs)    
 
+
 class User(object):
  
-    __metaclass__ = test_metaclass
+    __metaclass__ = MetaSingleton
     
     action_cooldown = {"att": 5,
                        "def": 10}
@@ -26,6 +42,6 @@ class User(object):
         if action in self.action_list:
             print "%s cooldown is %s" % (action, self.action_cooldown[action])
 
-#u = User()
+u = User()
 #u.action("att")
 
